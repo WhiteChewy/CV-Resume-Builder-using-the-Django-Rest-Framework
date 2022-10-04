a9e40f6763c1e8123c817d89fe853121be778ef0
Nikitos <pocketkurt@gmail.com>
2022-10-04 11:09:45 +0300

    Security fix
    minor changes

--- Resume/settings.py
+++ Resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j0ezh6ixfl6&+n771tt#05q44*6cs=c%3@d2wupy9g2u(cblvw'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
